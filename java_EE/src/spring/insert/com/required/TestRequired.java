5815478ae5a6838e8713e13a34cd8f15e5285e3e
Faithlmy <lmengyy@126.com>
2018-07-16 16:25:34 +0800

    required

---
+++ java_EE/src/spring/insert/com/required/TestRequired.java
@@ -0,0 +1,16 @@
+package com.required;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestRequired {
+
+	@Test
+	public void GetRequired() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("com/required/b.xml");
+		Student s = (Student) ac.getBean("student");
+		s.setAge(90);
+		System.out.println("name: " + s.getName() + ", age: " + s.getAge());
+	}
+}
